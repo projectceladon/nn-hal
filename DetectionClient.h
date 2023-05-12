1e1d2fa8b1c8dedfc83d4f4a96d8d0a9abd0aa90
Ratnesh Kumar Rai <ratnesh.kumar.rai@intel.com>
2023-05-12 17:28:02 +0530

    Added load model and data_type for remote infer
    loadmodel rpc call added after sending IR files
    Included data_type parameter for input data
    
    Tracked-On: OAM-109729
    Signed-off-by: Ratnesh Kumar Rai <ratnesh.kumar.rai@intel.com>

--- DetectionClient.h
+++ DetectionClient.h
@@ -8,6 +8,7 @@
 #include <log/log.h>
 #include <android-base/logging.h>
 #include "nnhal_object_detection.grpc.pb.h"
+#include "Driver.h"
 
 using grpc::Channel;
 using grpc::ClientContext;
@@ -32,9 +33,10 @@ public:
                     std::unique_ptr<ClientWriter<RequestDataChunks> >& writer);
 
     std::string sendIRs(bool& flag, const std::string& ir_xml, const std::string& ir_bin);
+    bool isModelLoaded(std::string fileName);
 
-    void add_input_data(std::string label, const uint8_t* buffer, std::vector<size_t> shape, uint32_t size);
-    void get_output_data(std::string label, uint8_t* buffer, std::vector<size_t> shape);
+    void add_input_data(std::string label, const uint8_t* buffer, std::vector<size_t> shape, uint32_t size, android::hardware::neuralnetworks::nnhal::OperandType operandType);
+    void get_output_data(std::string label, uint8_t* buffer, std::vector<size_t> shape, uint32_t expectedLength);
     void clear_data();
     std::string remote_infer();
     bool get_status();
